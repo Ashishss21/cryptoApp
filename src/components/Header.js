b47be8bc5bf98062eeb87f9a0c03f1f36ccfb26b
ashishss21 <ashish.sengar18@yahoo.com>
2022-01-16 10:18:19 +0530

    Step 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,28 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
 import { View, Text, Stylesheet, ScrollView } from "react-native";
-import Spinner from "react-native-loading-spinner-overlay";
+
+const Header = () =>{
+    return(
+        <View style={headerContainer}>
+            <Text style={header}>
+                Crypto App
+            </Text>
+        </View>
+    );
+}
+
+const styles= Stylesheet.create({
+    headerContainer:{
+        display:'flex',
+        marginTop:5,
+        alignItems:'center'
+    },
+    header:{
+        fontWeight:'bold',
+        fontSize:20,
+    }
+})
+
+const {headerContainer, header} = styles;
+
+export default Header;
